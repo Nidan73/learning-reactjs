f6eb03c672aac576a020a988a2954b059d1fc313
Nidan73 <Nidanalam73@gmail.com>
2026-01-16 21:13:58 +0600

    using useState

--- Module 37/react-core-concepts-part2/src/App.jsx
+++ Module 37/react-core-concepts-part2/src/App.jsx
@@ -1,14 +1,13 @@
 
+import { useState } from 'react';
 import './App.css'
-
+import Counter from './counter';
 function App() {
-  const handleclick = (num) => {
-    num += 5 ; 
-    alert(num)}
+   const [count , setCount]= useState(0)
   return (
     <>
      <h1>React Core Concepts Part 2 </h1>
-      <button onClick={() => handleclick(10)}>Click Me</button>
+      <Counter></Counter>
     </>
   )
 }
---
+++ Module 37/react-core-concepts-part2/src/counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export default function Counter(){
+
+    const counterStyle = {
+        border : '2px solid red'
+    }
+    const [count , setCount]= useState(0);
+   const handleClicked =() => {
+    const newCount = count + 5 ;
+    setCount(newCount);
+   }
+    return(
+        <div style={counterStyle}>
+            <h3>counter : {count}</h3>
+            <button onClick={handleClicked}>Add</button>
+        </div>
+    )
+}
