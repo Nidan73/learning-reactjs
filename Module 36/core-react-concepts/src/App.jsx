3c2ce579b606280fd2789893705b856d96d36476
Nidan73 <Nidanalam73@gmail.com>
2026-01-16 20:26:46 +0600

    loop used

--- Module 36/core-react-concepts/src/App.jsx
+++ Module 36/core-react-concepts/src/App.jsx
@@ -1,20 +1,26 @@
 import './App.css';
 import ToDo from './ToDo';
+import Singer from './singer';
 function App() {
-
+  const singers = [{ name : "Nidan" , age : 24 },{ name : "Tanjim" , age : 26 },
+    { name : "Nur" , age : 26 }];
   return (
     <>
     <h1>REACT CORE CONCEPTS</h1>
-     <Person name = "Nidan"></Person>
+     {/* <Person name = "Nidan"></Person>
       <Devloper name ="Nidan" age ="24" tech ="js"></Devloper>
       <Devloper name ="Nur" age ="26" tech ="HTML"></Devloper>
       <Devloper name ="Tanjim" age ="27" tech ="ECO"></Devloper>
       <ToDo tech ="js" isDone = {true}></ToDo>
       <ToDo tech ="css" isDone = {true}></ToDo>
-      <ToDo tech ="html" isDone = {false}></ToDo>
+      <ToDo tech ="html" isDone = {false}></ToDo> */}
+      {
+        singers.map(singer => <Singer singer={singer}></Singer>)
+      }
     </>
   )
 }
+
 
 function Devloper ({name , age = 0, tech}){
   return (
@@ -26,7 +32,7 @@ function Devloper ({name , age = 0, tech}){
     </div>
     </>
   )
-}
+} 
 function Person(props){
    const design = {
     color : 'red', 
