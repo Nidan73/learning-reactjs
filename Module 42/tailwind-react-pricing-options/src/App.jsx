513becd8ccc6abe0932807b39ddb5e8115c3d6b8
Nidan73 <Nidanalam73@gmail.com>
2026-01-22 21:18:02 +0600

    manually creating a responsive navbar with hamburg dropdown effect

--- Module 42/tailwind-react-pricing-options/src/App.jsx
+++ Module 42/tailwind-react-pricing-options/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+import Nav from './components/Navbar/Nav'
 
 function App() {
 
   return (
-    <>
-
+    <> 
+       {/* <DaisyNav></DaisyNav> */}
+       <Nav></Nav>
     </>
   )
 }
---
+++ Module 42/tailwind-react-pricing-options/src/components/Navbar/Nav.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import Link from './Link';
+import { Menu ,ListChevronsUpDown } from 'lucide-react';
+
+const navData = [
+  {
+    "id": 1,
+    "name": "Home",
+    "path": "/",
+    "url": "https://example.com/"
+  },
+  {
+    "id": 2,
+    "name": "About",
+    "path": "/about",
+    "url": "https://example.com/about"
+  },
+  {
+    "id": 3,
+    "name": "Services",
+    "path": "/services",
+    "url": "https://example.com/services"
+  },
+  {
+    "id": 4,
+    "name": "Projects",
+    "path": "/projects",
+    "url": "https://example.com/projects"
+  },
+  {
+    "id": 5,
+    "name": "Contact",
+    "path": "/contact",
+    "url": "https://example.com/contact"
+  }
+]
+const link = navData.map(route => <Link key={route.id} route ={route}></Link>)
+
+const Nav = () => {
+    const [toggle ,setToggle] = useState(true)
+    return (
+       <nav className='flex justify-between max-w-300 mx-auto'>
+           <div onClick={()=>setToggle(!toggle)} className='flex'>
+            {
+                toggle ? <Menu className='md:hidden'></Menu> : 
+                 <ListChevronsUpDown className='md:hidden'>
+                 </ListChevronsUpDown>
+            }
+            {
+                <ul className={`md:hidden absolute duration-500 ml-6 text-black bg-gray-400 
+                 ${toggle ? '-top-40' : 'top-5'}
+                `}>
+                      {link}
+                </ul>
+            }
+              <h3 className='ml-3'>My Navbar</h3>
+           </div>
+            <ul className='hidden md:flex'>
+            {
+                      link
+            }
+              </ul>
+          
+          <button>Sign in</button>
+
+
+
+
+
+
+
+
+
+        {/* <ul className='flex'>
+            {
+                navData.map(route => <li className='mr-10'>
+                    <a href={route.path}>{route.name}
+                    </a></li>)
+            }
+        </ul> */}
+           {/* <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/"></a></li>
+            <li><a href="/"></a></li>
+           </ul> */}
+       </nav>
+    );
+};
+
+export default Nav;
