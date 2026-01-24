0f5a47422e26f587d00af6cd8ba3624896c31c1b
Nidan73 <Nidanalam73@gmail.com>
2026-01-25 03:36:14 +0600

    Added nested routing

--- Module 43/react-router/src/main.jsx
+++ Module 43/react-router/src/main.jsx
@@ -6,13 +6,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
+import Root from './Components/Root/Root.jsx';
+import Home from './Components/Home/Home.jsx';
+import Mobile from './Components/Mobiles/Mobile.jsx';
+import Laptops from './Components/Laptops/Laptops.jsx';
 
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello from react router</div>,
+    Component : Root,
+    children : [
+      {index : true , Component : Home},
+      {path : 'Mobiles', Component : Mobile},
+      {path : 'Laptops', Component : Laptops}
+    ]
   },
 ]);
 createRoot(document.getElementById('root')).render(
