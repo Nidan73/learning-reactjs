8f8db29e82ca38e951b4d7797b8ecda171f5819a
Nidan73 <Nidanalam73@gmail.com>
2026-02-10 18:28:16 +0600

    React-firebase-auth-intregation

---
+++ Module 49/react-firebase-auth-integration/src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from "./Layout/Root";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "/register",
+        Component: Register,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>,
+);
